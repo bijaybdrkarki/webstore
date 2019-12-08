d87efec01db1359364947257348b50b2c7b26210
bijaybdrkarki <bijay.jhupro@gmail.com>
2019-12-08 16:51:37 -0500

    filter button js for click

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 let dropMenu = document.getElementById("menu");
-
+let filterBtn = document.querySelector(".filter-btn");
 // 
 function menuVisible()
 {
     document.getElementById("search").classList.toggle("visible");
     document.getElementById("cart").classList.toggle("invisible");
-    document.getElementById("logoPlace").classList.toggle("invisible");
+    // document.getElementById("logoPlace").classList.toggle("invisible");
     document.getElementById("mainmenu").classList.toggle("visible");
     let menuValue= document.getElementById("menu").innerHTML;
     if (menuValue == 'menu')
@@ -18,4 +18,23 @@ function menuVisible()
     }
     
 }
+function filterVisible(){
+    event.preventDefault();
+    document.querySelector(".filter-options").classList.toggle("visible");
+    let filterBtn = document.querySelector(".filter-btn i").innerHTML;
+    if (filterBtn == 'arrow_downward'){
+        document.querySelector(".filter-btn i").innerHTML = 'arrow_upward';
+    }else{
+        document.querySelector(".filter-btn i").innerHTML = 'arrow_downward';
+    }
+}
+
+
+
+
+
+
+
+
+filterBtn.addEventListener("click",filterVisible);
 dropMenu.addEventListener("click", menuVisible);
